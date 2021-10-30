9f18331af6f5b5dd364b6d9dd5dd59388c77d63a
Filowannabe <felipe.corredor.1249eam.edu.co>
2021-10-29 20:33:07 -0500

    Implementing async crypted password

--- src/utils/security/securityUtils.ts
+++ src/utils/security/securityUtils.ts
@@ -4,11 +4,11 @@ import bcrypt from 'bcrypt'
 
 export const hashSomePassowrd = async (password: string) => {
     const saltRounds = 12
-    const passwordCrypted = await bcrypt.hashSync(password, saltRounds)
+    const passwordCrypted = await bcrypt.hash(password, saltRounds)
     return passwordCrypted
 }
 
 export const comparePassword = async (password: string, hashedPassword: string) => {
-    const passwordCrypted = await bcrypt.compareSync(password, hashedPassword)
+    const passwordCrypted = await bcrypt.compare(password, hashedPassword)
     return passwordCrypted
 }
