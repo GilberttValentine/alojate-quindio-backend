eeaaebfe28425c7e9b4adda5d0020b3b20610f18
ErrorHyperion <juan.villanueva.1447@eam.edu.co>
2021-11-22 20:30:28 -0500

    Deleting Console.log()

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -61,11 +61,6 @@ export const createHost = async (id: number, languagesIdList: number[]) => {
 
 export const createGuest = async (id: number, stratum: number, studyLevelId: number, civilStatusId: number) => {
 
-    console.log(id)
-    console.log(stratum)
-    console.log(studyLevelId)
-    console.log(civilStatusId)
-    
     const userToFind = await UserRepository.findById(id);
 
     if (!userToFind) throw new NotFoundError('User not founded')
